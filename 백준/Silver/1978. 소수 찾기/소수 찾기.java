fd2849da8c46d7e33969a98c9e3c3544ab2bddf6
soeun135 <84930396+soeun135@users.noreply.github.com>
2023-02-24 18:21:45 +0900

    [Silver V] Title: 소수 찾기, Time: 228 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+class Main{
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int cnt = scanner.nextInt();
+        ArrayList<Integer> so=new ArrayList();
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+        for(int i=1;i<=cnt;i++){//입력한 숫자 갯수만큼 반복
+            int num = scanner.nextInt();
+            int count=0;
+            for(int j=2;j<=num;j++) {
+                if(num%j==0) {
+                    count++;
+               hashMap.put(num,count);
+                }
+            }
+        }
+        for(Integer key :   hashMap.keySet()) {
+        	if(hashMap.get(key)==1) 
+        		so.add(key);
+        }
+                System.out.println(so.size());        
+        scanner.close();
+    }
+}
