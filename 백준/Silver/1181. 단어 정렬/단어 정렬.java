5ba4fb117d8b3100901e18f77bc188a4513a6ec8
soeun135 <84930396+soeun135@users.noreply.github.com>
2023-03-06 22:08:42 +0900

    [Silver V] Title: 단어 정렬, Time: 380 ms, Memory: 27828 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,17 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
 
 class Main{
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int num = scanner.nextInt();
+		int num = Integer.parseInt(br.readLine());
 		String[] word = new String[num];
 		
 		//scanner.nextLine();
 		for(int i=0;i<num;i++) {
-			word[i] = scanner.next();
+			word[i] = br.readLine();
 		}
 		Arrays.sort(word, new Comparator<String>() {
 			@Override
@@ -32,6 +34,6 @@ class Main{
 			}
 		}
 			System.out.println(sb);
-		scanner.close();
+		//scanner.close();
 	}
 }
