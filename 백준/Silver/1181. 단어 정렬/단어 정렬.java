f60ea511a65cc991df28e4a68c07fee4685a441d
soeun135 <84930396+soeun135@users.noreply.github.com>
2023-03-06 21:47:22 +0900

    [Silver V] Title: 단어 정렬, Time: 824 ms, Memory: 61624 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,44 +1,34 @@
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.Comparator;
- 
- 
-public class Main {
+
+class Main{
 	public static void main(String[] args) {
-    
-		Scanner in = new Scanner(System.in);
- 
-		int N = in.nextInt();
-		String[] arr = new String[N];
- 
-		in.nextLine();	// 개행 버림
- 
-		for (int i = 0; i < N; i++) {
-			arr[i] = in.nextLine();
-		}
+		Scanner scanner = new Scanner(System.in);
 		
-		Arrays.sort(arr, new Comparator<String>() {
-			public int compare(String s1, String s2) {
-				// 단어 길이가 같을 경우 
-				if (s1.length() == s2.length()) {
-					return s1.compareTo(s2);
-				} 
-				// 그 외의 경우 
-				else {
-					return s1.length() - s2.length();
+		int num = scanner.nextInt();
+		String[] word = new String[num];
+		
+		scanner.nextLine();
+		for(int i=0;i<num;i++) {
+			word[i] = scanner.nextLine();
+		}
+		Arrays.sort(word, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				if(o1.length() == o2.length()) {
+					//단어 길이가 같으면 사전 순
+					return o1.compareTo(o2);
 				}
+				else return o1.length() - o2.length();
 			}
 		});
- 
- 
-		System.out.println(arr[0]);
-		
-		for (int i = 1; i < N; i++) {
-			// 중복되지 않는 단어만 출력
-			if (!arr[i].equals(arr[i - 1])) {
-				System.out.println(arr[i]);
+		System.out.println(word[0]);
+		for(int i=1; i< num; i++) {
+			if(!word[i].equals(word[i-1])) {
+				System.out.println(word[i]);
 			}
 		}
+		scanner.close();
 	}
- 
 }
