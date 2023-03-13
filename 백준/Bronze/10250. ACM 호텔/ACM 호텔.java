de535fa0f8a9e4a0a8b80ab6227a0c4105f60628
soeun135 <84930396+soeun135@users.noreply.github.com>
2023-03-13 21:37:05 +0900

    [Bronze III] Title: ACM 호텔, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -8,33 +8,17 @@ class Main{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int num = Integer.parseInt(br.readLine());
-		int input[][] = new int[num][3];
-		StringBuilder sb= new StringBuilder();
-		int X=0;
-		int Y=0;
-		int h=0;
-		int w=0;
-		int n=0;
+		StringBuilder sb = new StringBuilder();
 		for(int i=0;i<num;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			input[i][0] = Integer.parseInt(st.nextToken());
-			input[i][1] = Integer.parseInt(st.nextToken());
-			input[i][2] = Integer.parseInt(st.nextToken());
-		}
-		for(int i=0;i<num;i++) {
-			h = input[i][0];
-			w = input[i][1];
-			n = input[i][2];
-			if(n % h==0){//층수구하기
-				Y=h*100;
-				X = n/h;
-			}
+			int h = Integer.parseInt(st.nextToken());
+			st.nextToken();
+			int n = Integer.parseInt(st.nextToken());
+			
+			if(n % h==0)//층수구하기
+				sb.append(h*100+n/h).append("\n");
 			else
-			{
-				Y=(n%h)*100;
-				X = (n/h)+1;
-			}
-			sb.append(Y+X).append("\n");
+				sb.append((n%h)*100 +((n/h)+1)).append("\n");
 		}
 		System.out.println(sb);
 	}
