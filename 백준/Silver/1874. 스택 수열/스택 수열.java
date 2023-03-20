a3686b116a46cb5b3be7118f3dbd68c6fbd8e1b1
soeun135 <84930396+soeun135@users.noreply.github.com>
2023-03-20 21:24:57 +0900

    [Silver II] Title: 스택 수열, Time: 1852 ms, Memory: 27460 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int num = Integer.parseInt(br.readLine());
+		int arr[] = new int [num];
+		List <Integer>stack = new ArrayList<>();
+		StringBuilder sb = new StringBuilder();
+		int i=2;
+		int j=0;
+		int count =0;
+		String str = " ";
+		for(int k=0;k<num;k++) {
+			arr[k] = Integer.parseInt(br.readLine());
+		}
+		stack.add(1);
+		sb.append("+\n"); //일단 무조건 1은 들어가고 시작해야돼
+
+			while(count != num) {
+				//stack empty
+				if(stack.isEmpty()) {//스택이 비어있다면
+					stack.add(i);
+					sb.append("+\n");
+					i++;
+				}
+				//stack을 만들 수가 없을 때
+				if(stack.get(stack.size()-1) >arr[j]) {
+					str = "NO";
+					break;
+				}
+				//push
+				if(arr[j] != stack.get(stack.size()-1) ) {//수열과 1부터 n까지 증가하는 수가 같지않으면
+					stack.add(i); //다음수를 스택에 넣음
+					sb.append("+\n");
+					i++; //1부터 증가하는 
+				}
+				//pop
+				else {//스택의 맨 위에 수와 수열의 수가 같으면
+					if(!stack.isEmpty()) {
+						stack.remove(stack.remove(stack.size()-1)); //스택에서 pop
+						sb.append("-\n");
+						count++; //숫자가 몇 개나 pop돼서 나왔나(==수열과 일치하는 수를 몇 개 얻었는지)
+						j++; //수열의 다음 수랑 비교해야되니까 인덱스값 1증가
+					}
+						
+			}
+		}
+			if(str.equals("NO"))
+				System.out.println(str);
+			else
+				System.out.print(sb);
+	}
+}
