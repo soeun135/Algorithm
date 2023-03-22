3d4c89c040bb1d7ac48d0df42d4387c57319d235
soeun135 <84930396+soeun135@users.noreply.github.com>
2023-03-22 19:20:50 +0900

    [Silver III] Title: 소수 구하기, Time: 336 ms, Memory: 25552 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,15 +1,14 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 class Main{
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int m=0;
 		int n = 0;
 		
@@ -31,7 +30,9 @@ class Main{
 		}
 		for(int i=m;i<=n;i++) {
 			if(arr[i] == 0)
-				System.out.println(i);
+				bw.write(i+"\n");
 		}
+		bw.flush();
+		bw.close();
 	}
 }
