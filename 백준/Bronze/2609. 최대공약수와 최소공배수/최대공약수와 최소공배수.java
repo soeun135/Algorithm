826930a4448a29dbe76c67d986d3abfdc4bdd6d0
soeun135 <84930396+soeun135@users.noreply.github.com>
2023-03-31 20:13:05 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 152 ms, Memory: 16088 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -3,35 +3,40 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
+//유클리드의 호제법으로 풀었다
 class Main{
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int num1 = Integer.parseInt(st.nextToken());
 		int num2 = Integer.parseInt(st.nextToken());
 		int min = 0;
-		int maxv = 0;
-		int minv = 0;
+		int max = 0;
+		int GCF=0;
 		if(num1>num2) {
+			max = num1;
 			min = num2;
 		}
 		else {
+			max = num2;
 			min = num1;
 		}
-		for(int i=min;i>=1;i--) {
-			if(num1%i == 0 && num2%i ==0) {
-				maxv = i;
-				minv = i*(num1/i) * (num2/i);
-				break;
+		while(GCF ==0) { //최대공약수
+			int R = max %min;
+			if(R == 0)
+				GCF = min;
+			else { //나머지가 있으면
+				max = min;
+				min = R;
 			}
-		}
-		StringBuilder sb = new StringBuilder();
-		sb.append(maxv+"\n"+minv);
-		System.out.println(sb);
+		}	
+		//최소공배수 LCM
+		int LCM = num1*num2/GCF;
+		System.out.println(GCF+"\n"+LCM);
     }
 }
