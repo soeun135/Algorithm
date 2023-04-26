f1daa0b958c9cb41ea1dbb1b97a8732cb5849710
soeun135 <84930396+soeun135@users.noreply.github.com>
2023-04-26 20:35:32 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 120 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,10 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MulticastSocket;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.StringTokenizer;
 
 class Main{
@@ -16,31 +12,11 @@ class Main{
     	
     	int count =0;
    
-    	while(str.length() >0){
-    		boolean ch = false;
-    		String alpha ="";
-    		String beta = "";
-    		if(str.length()>=2)
-    			alpha = str.substring(0,2);
-    		if(str.length()>=3)
-    			beta = str.substring(0,3);
-    		for(int j=0;j<arr.length;j++) {
-    			if(alpha.equals(arr[j])) {
-    				count++;
-    				str = str.substring(2);
-    				ch=true;
-    			}
-    			else if(beta.equals(arr[j])) {
-    				count++;
-    				str = str.substring(3);
-    				ch=true;
-    			}
+    	for(int i=0;i<arr.length;i++) {
+    		if(str.contains(arr[i])) {
+    			str = str.replace(arr[i],"*");
     		}
-    			if(ch == false) { //배열에 저장된 크로아티아 알파벳이 아니라면
-    				count++;
-    				str = str.substring(1);
-    			}
     	}
-    	System.out.println(count);
+    	System.out.println(str.length());
 	}
 }
