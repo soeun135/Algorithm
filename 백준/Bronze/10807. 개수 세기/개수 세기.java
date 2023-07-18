e2c7a6714a8c49dbf2a25636c6ae89ba5741b605
soeun135 <84930396+soeun135@users.noreply.github.com>
2023-07-18 16:04:39 +0900

    [Bronze V] Title: 개수 세기, Time: 120 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,29 +1,31 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 class Main{
 	public static void main(String[] args) throws IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
       
-    	int num = Integer.parseInt(br.readLine());
-    	int arr[] = new int[num];
+    	
+    	int N = Integer.parseInt(br.readLine());//N개의 정수
+    	
+    	int arr[] = new int[N];
+
     	StringTokenizer st = new StringTokenizer(br.readLine());
-    	int count =0;
-    	for(int i =0;i<num;i++) {
+    	for(int i=0;i<N;i++) {
     		arr[i] = Integer.parseInt(st.nextToken());
     	}
-    	int search = Integer.parseInt(br.readLine());
-    	for(int i =0;i<num;i++) 
-    		if(arr[i] == search) {
-			count++;
-    		}
-       bw.write(Integer.toString(count));
-       bw.flush();
-       bw.close();
+    	int M = Integer.parseInt(br.readLine());
+    	int count = 0;
+    	for(int i =0;i<arr.length;i++) {
+    		if(M == arr[i]) 
+    			count++;
+    	}
+    		
+    	System.out.print(count);
     }
 }
