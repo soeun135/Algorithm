272eb23cd59907ce6f693c78ae04e7b9647fda4e
soeun135 <84930396+soeun135@users.noreply.github.com>
2023-07-24 03:12:12 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.00 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+class Solution {
+     static class Job implements Comparable<Job>{
+        int request;
+        int jobTime;
+
+        public Job(int request, int jobTime) {
+            this.request = request;
+            this.jobTime = jobTime;
+        }
+        @Override
+        public int compareTo(Job o) {
+            return this.jobTime - o.jobTime;
+        }
+    }
+    public static int solution(int[][] jobs) {
+        ArrayList <Job> jobList = new ArrayList<>();
+        PriorityQueue <Job> waitingQueue = new PriorityQueue<>();
+
+        Arrays.sort(jobs, (o1, o2) -> {
+            if(o1[0] == o2[0]){
+                return Integer.compare(o1[1] , o2[1]);
+            } else{
+                return Integer.compare(o1[0], o2[0]);
+            }
+        });
+
+        for (int i = 0; i < jobs.length; i++) {
+            jobList.add(new Job(jobs[i][0], jobs[i][1]));
+        }
+        int prev_finish = 0;
+        int answer = 0;
+
+        while(!jobList.isEmpty() || !waitingQueue.isEmpty()){
+            boolean isNew = false;
+
+            Iterator <Job> iter = jobList.iterator();
+            while(iter.hasNext()) {
+                Job job = iter.next();
+                if (job.request > prev_finish) break;
+
+                waitingQueue.add(job);
+                iter.remove();
+            }
+                if(waitingQueue.size() == 0){
+                    waitingQueue.add(jobList.get(0));
+                    jobList.remove(0);
+
+                    isNew = true;
+                }
+
+                Job wJob = waitingQueue.poll();
+                if(isNew){
+                    answer += wJob.jobTime;
+                    prev_finish = wJob.request + wJob.jobTime;
+                } else{
+                    answer += ((prev_finish - wJob.request) + wJob.jobTime);
+                    prev_finish += wJob.jobTime;
+                }
+            }
+        return (int)(answer / jobs.length);
+
+    }
+}
