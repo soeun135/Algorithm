30cddb37cdfe76d6aee5be22013b55ad78512263
soeun135 <84930396+soeun135@users.noreply.github.com>
2023-07-27 18:15:30 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -3,17 +3,13 @@ import java.util.*;
 class Solution {
     public static int[] solution(int[] arr, int[][] queries) {
         int[] answer = {};
-        ArrayList<Integer> list = new ArrayList();
-        for(int item : arr) {
-            list.add(item);
+      
+        for(int x[] : queries) {
+            int tmp = arr[x[0]];
+            arr[x[0]] = arr[x[1]];
+            arr[x[1]] = tmp;
         }
-        for (int i = 0; i < queries.length; i++) {
-            int tmp1  = list.get(queries[i][0]);
-            int tmp2 = list.get(queries[i][1]);
-            list.set(queries[i][1], tmp1);
-            list.set(queries[i][0], tmp2);
-        }
-        answer = list.stream().mapToInt(x -> x).toArray();
+        answer = arr;
         return answer;
     }
 }
