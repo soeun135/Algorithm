a4028847fa1620c812ec2d300a8d6cf93f64027d
soeun135 <84930396+soeun135@users.noreply.github.com>
2023-08-05 23:07:00 +0900

    [level 0] Title: 이진수 더하기, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static String solution(String bin1, String bin2) {
+        String answer = "";
+        int n1 = Integer.parseInt(bin1,2);
+        int n2 = Integer.parseInt(bin2,2);
+        answer = Integer.toBinaryString(n1+n2);
+        return answer;
+    }
+}
