709b1dbf89dac8d0e976218c4689341cdeb90236
soeun135 <84930396+soeun135@users.noreply.github.com>
2023-08-06 08:50:59 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 584 ms, Memory: 30424 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -3,34 +3,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Main {
+public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int arr[][] = new int[n][m];
+        int dp[][] = new int[n + 1][m + 1];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 1; j <= m; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + Integer.parseInt(st.nextToken());
             }
         }
         int k = Integer.parseInt(br.readLine());
-
         for (int i = 0; i < k; i++) {
             st = new StringTokenizer(br.readLine());
-            int p = Integer.parseInt(st.nextToken())-1;
-            int q = Integer.parseInt(st.nextToken())-1;
-            int x = Integer.parseInt(st.nextToken())-1;
-            int y = Integer.parseInt(st.nextToken())-1;
+            int p = Integer.parseInt(st.nextToken());
+            int q = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
             int answer = 0;
-            for (int j = p; j <= x; j++) {
-                for (int l = q; l <= y ; l++) {
-                    answer += arr[j][l];
-                }
-            }
+
+            answer = dp[x][y] - dp[p-1][y] - dp[x][q - 1] + dp[p - 1][q - 1];
             System.out.println(answer);
         }
     }
