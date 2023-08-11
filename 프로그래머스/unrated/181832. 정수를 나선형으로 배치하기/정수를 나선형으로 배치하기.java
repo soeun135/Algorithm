0bffdd963cef702be7afbddd8cf860989fb03893
soeun135 <84930396+soeun135@users.noreply.github.com>
2023-08-11 18:02:49 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 0.08 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -1,25 +1,40 @@
 class Solution {
-    public int[][] solution(int n) {
-        int[][] answer = new int[n][n];
-        int dx[] = {-1, 0, 1, 0};
-        int dy[] = {0, 1, 0, -1};
-        int x = 0, y = 0, d = 1;
-        int k = n * n;
-        int cnt = 1;
-        while (cnt < k) {
-            int nx = x + dx[d];
-            int ny = y + dy[d];
-            
-            if (nx <0 || nx >= n || ny < 0 || ny >= n || answer[nx][ny] > 0) {
-                d = (d + 1) % 4;
-                continue;
+    public static int[][] solution(int n) {
+        int arr[][] = new int[n][n];
+
+        int d[][] = {{0,1},{1,0},{0,-1},{-1,0}};
+        int num = 1;
+        int curX = 0; int curY = 0;
+        arr[curX][curY] = num++;
+        boolean visited[][] = new boolean[n][n];
+        visited[curX][curY] = true;
+
+        while (num <= n * n) {
+            for (int i = 0; i < d.length; i++) {
+                while (true) {
+                    curX = curX + d[i][0];
+                    curY = curY + d[i][1];
+
+                    if (curX < 0 || curX >= n || curY < 0 || curY >=n) {
+                        //원래 좌표로 돌려줘야함.
+                        if (curX <0 || curX >= n) {
+                            curX -= d[i][0];
+                        } else {
+                            curY -= d[i][1];
+                        }
+                        break;
+                    }
+                    if (!visited[curX][curY]) {
+                        arr[curX][curY] = num++;
+                        visited[curX][curY] = true;
+                    } else {
+                        curX -= d[i][0];
+                        curY -= d[i][1];
+                        break;
+                    }
+                }
             }
-            answer[x][y] = cnt;
-            cnt ++;
-            x = nx;
-            y = ny;
         }
-        answer[x][y] = cnt;
-        return answer;
+        return arr;
     }
 }
