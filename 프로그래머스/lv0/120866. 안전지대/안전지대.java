ed1cc7106f6977b04159d0f3cf7f47a51909990d
soeun135 <84930396+soeun135@users.noreply.github.com>
2023-08-12 07:07:07 +0900

    [level 0] Title: 안전지대, Time: 0.05 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.java
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.java
@@ -1,28 +1,27 @@
 class Solution {
     public static int solution(int[][] board) {
+        int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
         int n = board.length;
         int answer = n * n;
         int cnt = 0;
-        int dr[] = {-1,-1,0,1,1,1,0,-1};
-        int dc[] = {0,1,1,1,0,-1,-1,-1};
-        for (int i = 0; i < n; i++) {
+        
+        for (int i = 0; i < n ; i++) {
             for (int j = 0; j < n; j++) {
                 if (board[i][j] == 1) {
                     cnt++;
-
-                    for (int k = 0; k < 8; k++) {
-                        int r = i + dr[k];
-                        int c = j + dc[k];
-
-                        if (r >= 0 && r < n && c >= 0 && c < n && board[r][c] == 0) {
-                            cnt ++;
-                            board[r][c] = 2;
+                    for (int k = 0; k < 8 ; k++) {
+                        int curX = i + dx[k];
+                        int curY = j + dy[k];
+                        
+                        if (curX >= 0 && curX < n && curY >= 0 && curY < n && board[curX][curY] == 0) {
+                            cnt++;
+                            board[curX][curY] = 2;
                         }
                     }
                 }
             }
         }
-
         return answer - cnt;
     }
 }
