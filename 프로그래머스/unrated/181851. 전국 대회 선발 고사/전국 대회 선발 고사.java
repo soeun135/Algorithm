b4d443d7523e5def42e0a1b1ae067a15bb4a92bf
soeun135 <84930396+soeun135@users.noreply.github.com>
2023-08-11 17:13:05 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 0.23 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -3,19 +3,19 @@ import java.util.*;
 class Solution {
     public static int solution(int[] rank, boolean[] attendance) {
         int answer = 0;
-        HashMap <Integer, Integer>map = new HashMap<>();
         ArrayList <Integer>list = new ArrayList<>();
-        for (int i = 0; i < rank.length ; i++) {
+        HashMap <Integer, Integer>map = new HashMap<>();
+        for (int i = 0; i < attendance.length ; i++) {
             if (attendance[i]) {
-                map.put(rank[i], i);
+                map.put(rank[i], i); //등수와 인덱스를 차례로 넣음.
                 list.add(rank[i]);
             }
         }
-        Collections.sort(list);
-
-        answer += map.get(list.get(0)) * 10000;
-        answer += map.get(list.get(1)) * 100;
-        answer += map.get(list.get(2));
-        return answer;
+        Collections.sort(list); //등수별 오름차순
+        int a = 10000 * map.get(list.get(0));
+        int b = 100 * map.get(list.get(1));
+        int c = map.get(list.get(2));
+        
+        return a + b + c;
     }
 }
