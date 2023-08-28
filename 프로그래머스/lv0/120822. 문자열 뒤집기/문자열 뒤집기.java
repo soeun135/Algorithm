9561b09416c0fc8cac8d85987ca4d6edf6345298
soeun135 <84930396+soeun135@users.noreply.github.com>
2023-08-28 21:48:42 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,10 @@
+//import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(my_string);
+        
+        return sb.reverse().toString();
+    }
+}
