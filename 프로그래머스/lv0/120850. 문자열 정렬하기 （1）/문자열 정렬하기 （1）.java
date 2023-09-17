92f4d35925942c3cb603b056cfe20c8f9cba9725
soeun135 <84930396+soeun135@users.noreply.github.com>
2023-09-17 18:42:07 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.69 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -2,15 +2,14 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String my_string) {
-        int[] answer = {};
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i = 0; i < my_string.length() ; i++) {
-            if (my_string.charAt(i) >= '0' && my_string.charAt(i) <= '9') {
-                list.add(Character.getNumericValue(my_string.charAt(i)));
-            }
-        }
-        Collections.sort(list);
         
-         return list.stream().mapToInt(x->x).toArray();
+        my_string = my_string.replaceAll("[^0-9]","");
+        int[] answer = new int[my_string.length()];
+        for(int i = 0; i < my_string.length() ; i++) {
+                answer[i] = Character.getNumericValue(my_string.charAt(i));
+        }
+        Arrays.sort(answer);
+        
+         return answer;
     }
 }
