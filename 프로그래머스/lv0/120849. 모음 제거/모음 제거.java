6f87238bca43a3d856fd5bc113774ce76bb1bd1e
soeun135 <84930396+soeun135@users.noreply.github.com>
2023-09-17 18:42:51 +0900

    [level 0] Title: 모음 제거, Time: 0.11 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -1,14 +1,7 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        for(int i = 0; i < my_string.length(); i++) {
-            char c = my_string.charAt(i);
-            if(c=='a' || c=='e' ||c=='i' ||c=='o' ||c=='u') {
-                continue;
-            } else {
-                answer += c;
-            }
-        }
-        return answer;
+        my_string = my_string.replaceAll("[aeiou]","");
+        return my_string;
     }
 }
