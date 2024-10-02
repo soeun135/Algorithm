213ded3b0d853f85fbbad476f4e0026f0715d1d0
soeun135 <84930396+soeun135@users.noreply.github.com>
2024-10-02 16:34:42 +0900

    [level 2] Title: 교점에 별 만들기, Time: 28.35 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/README.md
+++ 프로그래머스/2/87377. 교점에 별 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 교점에 별 만들기 - 87377 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87377?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87377) 
 
 ### 성능 요약
 
-메모리: 76.6 MB, 시간: 0.24 ms
+메모리: 89.2 MB, 시간: 28.35 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 14일 13:51:34
+2024년 10월 02일 16:34:31
 
 ### 문제 설명
 
--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 import java.util.List;
 
 class Solution {
-    private static class Point {
+    public static class Point {
         public final long x, y;
 
         private Point(long x, long y) {
@@ -11,67 +11,62 @@ class Solution {
             this.y = y;
         }
     }
+
+
+
     public String[] solution(int[][] line) {
 
         List<Point> points = new ArrayList<>();
-
         for (int i = 0; i < line.length; i++) {
             for (int j = i + 1; j < line.length; j++) {
                 Point intersection = intersection(line[i][0], line[i][1], line[i][2],
                         line[j][0], line[j][1], line[j][2]);
 
-                if (intersection != null)
+                if (intersection != null) {
                     points.add(intersection);
+                }
             }
         }
-        
-        Point min = getMinimumPoint(points);
-        Point max = getMaximumPoint(points);
-        
-        int width = (int) (max.y - min.y + 1);
-        int height = (int) (max.x - min.x + 1);
-        
-        char[][] arr = new char[width][height];
+        Point minimumPoint = getMinimumPoint(points);
+        Point maximumPoint = getMaximumPoint(points);
+
+        int height = (int) (maximumPoint.y - minimumPoint.y + 1);
+        int width = (int) (maximumPoint.x - minimumPoint.x + 1);
+
+        char[][] arr = new char[height][width];
         for (char[] row : arr) {
             Arrays.fill(row, '.');
         }
-        
+
         for (Point p : points) {
-            int x = (int) (p.x - min.x);
-            int y = (int) (max.y - p.y);
+            int x = (int) (p.x - minimumPoint.x);
+            int y = (int) (maximumPoint.y - p.y);
+            
             arr[y][x] = '*';
         }
+
         String[] answer = new String[arr.length];
         for (int i = 0; i < answer.length; i++) {
             answer[i] = new String(arr[i]);
         }
-        
         return answer;
     }
 
-    private Point intersection(long a1, long a2, long a3, long b1, long b2, long b3) {
-        double x = (double) ((a2 * b3) - (b2 * a3)) / ((a1 * b2) - (a2 * b1));
-        double y = (double) ((a3 * b1) - (a1 * b3)) / ((a1 * b2) - (a2 * b1));
-
-        if (x % 1 == 0 && y % 1 == 0) {
-            return new Point((long) x, (long) y);
-        }
-        return null;
-    }
-    
     private Point getMinimumPoint(List<Point> points) {
+        // 가장 작은 좌표 찾기
         long x = Long.MAX_VALUE;
         long y = Long.MAX_VALUE;
-        
+
         for (Point p : points) {
             if (p.x < x) x = p.x;
             if (p.y < y) y = p.y;
         }
-        
+
         return new Point(x, y);
     }
-    
+
     private Point getMaximumPoint(List<Point> points) {
+        // 가장 큰 좌표 찾기
         long x = Long.MIN_VALUE;
         long y = Long.MIN_VALUE;
 
@@ -79,7 +74,18 @@ class Solution {
             if (p.x > x) x = p.x;
             if (p.y > y) y = p.y;
         }
-
         return new Point(x, y);
+    }
+
+    private Point intersection(long a1, long b1, long c1, long a2, long b2, long c2) {
+
+        double x = (double) (b1 * c2 - b2 * c1) / (a1 * b2 - a2 * b1);
+        double y = (double) (a2 * c1 - a1 * c2) / (a1 * b2 - a2 * b1);
+
+        if (x % 1 != 0 || y % 1 != 0) {
+            return null;
+        }
+
+        return new Point((long) x, (long) y);
     }
 }
