0264e3e33a1083130a0aa393e83217d71bd3b58f
soeun135 <84930396+soeun135@users.noreply.github.com>
2024-11-05 20:55:16 +0900

    [Gold V] Title: 토마토, Time: 572 ms, Memory: 118376 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -2,31 +2,29 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static int M, N, H;
     static int[][][] tomato;
     static boolean[][][] visited;
-    static int[][] d = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {0, 0}, {0, 0}};
-    static int[] dH = {0, 0, 0, 0, -1, 1};
-    static int N, M, H;
     static int day = 0;
 
-    static Queue<Point> q = new LinkedList<>();
+    static int[] dx = {0, 0, -1, 1, 0, 0};
+    static int[] dy = {-1, 1, 0, 0, 0, 0};
+    static int[] dh = {0, 0, 0, 0, -1, 1};
 
+    static class Point {
+        int x, y, h;
 
-    public static class Point {
-        int h, x, y, day;
-
-        public Point(int h, int x, int y, int day) {
-            this.h = h;
+        public Point(int h, int x, int y) {
             this.x = x;
             this.y = y;
-            this.day = day;
+            this.h = h;
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
         H = Integer.parseInt(st.nextToken());
@@ -34,17 +32,11 @@ public class Main {
         tomato = new int[H][N][M];
         visited = new boolean[H][N][M];
 
-        for (int h = 0; h < H; h++) {
-            for (int x = 0; x < N; x++) {
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < N; j++) {
                 st = new StringTokenizer(br.readLine());
-
-                for (int y = 0; y < M; y++) {
-                    tomato[h][x][y] = Integer.parseInt(st.nextToken());
-
-                    if (tomato[h][x][y] == 1) {
-                        q.offer(new Point(h, x, y, 0));
-                        visited[h][x][y] = true;
-                    }
+                for (int k = 0; k < M; k++) {
+                    tomato[i][j][k] = Integer.parseInt(st.nextToken());
                 }
             }
         }
@@ -52,37 +44,54 @@ public class Main {
             System.out.println(day);
             return;
         }
-
         bfs();
 
-        if (!done()) {
+        if (done()) {
+            System.out.println(day);
+        } else {
             System.out.println(-1);
-            return;
         }
-        System.out.println(day);
     }
 
     public static void bfs() {
+        Queue<Point> q = new LinkedList<>();
 
-        while (!q.isEmpty()) {
-            Point cur = q.poll();
-
-            for (int i = 0; i < 6; i++) {
-                int dh = cur.h + dH[i];
-                int dx = cur.x + d[i][0];
-                int dy = cur.y + d[i][1];
-
-                if (dx < 0 || dy < 0 || dh < 0 || dh >= H || dx >= N || dy >= M || visited[dh][dx][dy] || tomato[dh][dx][dy] == -1) {
-                    continue;
-                }
-
-                if (tomato[dh][dx][dy] == 0) {
-                    tomato[dh][dx][dy] = 1;
-                    q.offer(new Point(dh, dx, dy, cur.day + 1));
-                    visited[dh][dx][dy] = true;
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < M; k++) {
+                    if (tomato[i][j][k] == 1) {
+                        q.offer(new Point(i, j, k));
+                        visited[i][j][k] = true;
+                    }
                 }
             }
-            day = cur.day;
+        }
+        while (!q.isEmpty()) {
+
+            int size = q.size();
+            boolean flag = false;
+
+            for (int i = 0; i < size; i++) {
+                Point cur = q.poll();
+
+                for (int j = 0; j < 6; j++) {
+                    int nh = cur.h + dh[j];
+                    int nx = cur.x + dx[j];
+                    int ny = cur.y + dy[j];
+
+                    if (nh < 0 || nx < 0 || ny < 0 || nh >= H || nx >= N || ny >= M || visited[nh][nx][ny] || tomato[nh][nx][ny] == -1) {
+                        continue;
+                    }
+
+                    if (tomato[nh][nx][ny] == 0) {
+                        tomato[nh][nx][ny] = 1;
+                        visited[nh][nx][ny] = true;
+                        q.offer(new Point(nh, nx, ny));
+                        flag = true;
+                    }
+                }
+            }
+            if (flag) day++;
         }
     }
 
