95a51713cdc834c18743087655ff4fe5deaaa2de
soeun135 <84930396+soeun135@users.noreply.github.com>
2024-11-19 00:22:02 +0900

    [Silver III] Title: N과 M (8), Time: 148 ms, Memory: 18720 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N, M;
+    static StringBuilder sb = new StringBuilder();
+    static int[] num;
+    static int[] selected;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        num = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(num);
+        selected = new int[M];
+        dfs(0, 0);
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int start, int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(selected[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            selected[depth] = num[i];
+            dfs(i, depth + 1);
+        }
+    }
+}
