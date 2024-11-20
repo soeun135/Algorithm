6ed0a98d47c848508cede4e7331cafe4e5770fdc
soeun135 <84930396+soeun135@users.noreply.github.com>
2024-11-20 21:02:44 +0900

    [Silver II] Title: N과 M (9), Time: 160 ms, Memory: 20192 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,11 +1,13 @@
-
-import java.util.*;
-import java.io.*;
-import java.util.stream.Collectors;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
-    static Set<String> set = new HashSet<>();
     static StringBuilder sb = new StringBuilder();
     static int[] num;
     static int[] selected;
@@ -27,27 +29,29 @@ public class Main {
 
         selected = new int[M];
         visited = new boolean[N];
-        dfs(0, 0);
+        dfs(0);
 
         System.out.println(sb);
     }
 
-    public static void dfs(int start, int depth) {
+    public static void dfs(int depth) {
         if (depth == M) {
-            String result = Arrays.stream(selected)
-                    .mapToObj(String::valueOf)
-                    .collect(Collectors.joining(" "));
-            if(!set.contains(result)) {
-                set.add(result);
-                sb.append(result).append("\n");
+            for (int i = 0; i < M; i++) {
+                sb.append(selected[i]).append(" ");
             }
+            sb.append("\n");
             return;
         }
+
         for (int i = 0; i < N; i++) {
+            if (i > 0 && num[i - 1] == num[i] && !visited[i - 1]) {
+                continue;
+            }
+
             if (!visited[i]) {
                 selected[depth] = num[i];
                 visited[i] = true;
-                dfs(i + 1, depth + 1);
+                dfs(depth + 1);
                 visited[i] = false;
             }
         }
