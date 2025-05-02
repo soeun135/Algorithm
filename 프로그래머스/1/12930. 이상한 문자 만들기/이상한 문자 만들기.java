e92b647e1689c9763ecc97d68f09a068b0c4e816
soeun135 <84930396+soeun135@users.noreply.github.com>
2025-05-02 17:53:59 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.19 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,21 +1,24 @@
 class Solution {
-     public static String solution(String s) {
+     public String solution(String s) {
 
         StringBuilder sb = new StringBuilder();
         boolean toUpper = true;
+
         for (char c : s.toCharArray()) {
-            if (Character.isSpaceChar(c)) {
+            if (!Character.isAlphabetic(c)) {
+                //공백처리
                 sb.append(c);
                 toUpper = true;
             } else {
+                //알파벳 변환
                 if (toUpper) {
                     sb.append(Character.toUpperCase(c));
-                } else sb.append(Character.toLowerCase(c));
-                            toUpper = !toUpper;
-
+                } else {
+                    sb.append(Character.toLowerCase(c));
+                }
+                toUpper = !toUpper;
             }
         }
-       
         return sb.toString();
     }
 }
