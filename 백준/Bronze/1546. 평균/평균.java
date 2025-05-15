22651cf5525bdaa99f614199ffdeeef6708bfc0e
soeun135 <84930396+soeun135@users.noreply.github.com>
2025-05-16 00:15:09 +0900

    [Bronze I] Title: 평균, Time: 104 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,22 +1,29 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-class Main{
-    public static void main(String args[]){
-        Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
-       double score[] = new double[num];
-        int max=0;
-        double sum=0;
-        for(int i=0;i<num;i++){
-            score[i]=scanner.nextInt();
-            if(max<score[i])
-                max=(int)score[i];   
+class Main {
+    public static void main(String[] args) throws IOException{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] score = new int[N];
+        double[] newScore = new double[N];
+        double sum = 0;
+        int max = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < N; i++) {
+            score[i] = Integer.parseInt(st.nextToken());
+            if (max < score[i]) {
+                max = score[i];
+            }
         }
-        for(int i=0;i<num;i++){
-            score[i]= (score[i]/max*100);
-            sum+=score[i];
+        
+        for (int i = 0; i < N; i++) {
+            newScore[i] = (double) score[i] / max * 100;
+            sum += newScore[i];
         }
-        System.out.println(sum/num);
-        scanner.close();
+        System.out.println(sum / N);
     }
 }
