a69cf48862e0ba953808ab6863ca983b5fb44345
soeun135 <84930396+soeun135@users.noreply.github.com>
2025-06-21 22:18:04 +0900

    [Gold IV] Title: 이분 그래프, Time: 824 ms, Memory: 269612 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -1,112 +1,70 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-    static int k;
-    static int V, E;
-    static int u, v;
-
-    static List[] graph;
-    static int[] visit;
+class Main {
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static int[] check;
+    static boolean flag = true;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine()); //테스트 케이스 갯수
 
-        k = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < k; i++) {
+        for (int i = 0; i < K; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
+            int V = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
 
-            V = Integer.parseInt(st.nextToken());
-            E = Integer.parseInt(st.nextToken());
+            graph = new ArrayList[V + 1];
+            visited = new boolean[V + 1];
+            check = new int[V + 1];
 
-            graph = new List[V + 1];
-            visit = new int[V + 1];
-
-            for (int j = 0; j <= V; j++) {
+            for (int j = 1; j < V + 1; j++) {
                 graph[j] = new ArrayList<>();
             }
-
             for (int j = 0; j < E; j++) {
                 st = new StringTokenizer(br.readLine());
 
-                u = Integer.parseInt(st.nextToken());
-                v = Integer.parseInt(st.nextToken());
+                int u = Integer.parseInt(st.nextToken());
+                int v = Integer.parseInt(st.nextToken());
 
                 graph[u].add(v);
                 graph[v].add(u);
             }
-
-//            visit[1] = 1;
-//            dfs(1);
-
-            bfs();
-//            if (chk) {
-//                System.out.println("YES");
-//            } else {
-//                System.out.println("NO");
-//            }
-        }
-    }
-
-    static boolean flag = false;
-    static boolean chk = true;
-
-    public static void bfs() {
-        Queue<Integer> q = new LinkedList<>();
-
-        for (int i = 1; i <= V; i++) {
-            if (visit[i] == 0) {
-                q.offer(i);
-                visit[i] = 1;
-            }
-
-            while (!q.isEmpty()) {
-                int now = q.poll();
-
-                for (int j = 0; j < graph[now].size(); j++) {
-
-                    if (visit[(int) graph[now].get(j)] == 0) {
-                        q.offer((int) graph[now].get(j));
-                    }
-                    if (visit[now] == visit[(int) graph[now].get(j)]) {
-                        System.out.println("NO");
-                        return;
-                    }
-
-                    if (visit[(int) graph[now].get(j)] == 0) {
-                        if (visit[now] == 1) {
-                            visit[(int) graph[now].get(j)] = 2;
-                        } else {
-                            visit[(int) graph[now].get(j)] = 1;
-                        }
-                    }
+            // 모든 노드에 대해 dfs 실행
+            for (int j = 1; j < V + 1; j++) {
+                if (!visited[j]) {
+                    check[j] = 1;
+                    dfs(j);
                 }
+                if (!flag) {
+                    break;
+                }
+                //dfs 결과가 이분 그래프 아니면 반복 종료
             }
+            //이분그래프 결과 출력
+            if (flag) System.out.println("YES");
+            else System.out.println("NO");
+            flag = true;
         }
-        System.out.println("YES");
     }
 
-    public static void dfs(int start) {
+    private static void dfs(int cur) {
+        //현재노드 출력?
+        visited[cur] = true;
 
-        for (int i = 0; i < graph[start].size(); i++) {
-            //방문했으면 빠르게
-            if (visit[(int) graph[start].get(i)] != 0) {
-                if (visit[start] == visit[(int) graph[start].get(i)]) {
-                    chk = false;
+        for (int i = 0; i < graph[cur].size(); i++) {
+            int next = (int) graph[cur].get(i);
+            if (!visited[next]) {
+                check[next] = check[cur] == 1 ? 2 : 1;
+                dfs(next);
+            } else { //방문했을 때
+                if (check[cur] == check[next]) {
+                    flag = false;
                     return;
                 }
             }
-            if (flag) {
-                visit[(int) graph[start].get(i)] = 1;
-            } else {
-                visit[(int) graph[start].get(i)] = 2;
-            }
-            flag = !flag;
-            dfs((int) graph[start].get(i));
         }
-
     }
-
 }
-
