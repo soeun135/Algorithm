6c0cc3500185e4ebee0306520c7dcd67426948ba
soeun135 <84930396+soeun135@users.noreply.github.com>
2025-06-26 09:07:21 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1124 ms, Memory: 70628 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,49 +1,53 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
-class Main{
+class Main {
     static int N;
-    static ArrayList <Integer>[] list;
-    static int []parentsArr;
-    public static void main(String args[]) throws IOException{
+
+    static int[] parent;
+    static List<Integer>[] tree;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        parentsArr = new int[N + 1];
 
-        list = new ArrayList[N + 1];
-        for (int i = 1; i <= N ; i++) {
-            list[i] = new ArrayList<>();
+        tree = new List[N + 1];
+        parent = new int[N + 1];
+
+        for (int i = 1; i < N + 1; i++) {
+            tree[i] = new ArrayList<>();
         }
         for (int i = 0; i < N - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            list[x].add(y);
-            list[y].add(x);
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            tree[s].add(e);
+            tree[e].add(s);
         }
         bfs(1);
-        for (int i = 2; i <= N ; i++) {
-            System.out.println(parentsArr[i]);
-        }
     }
-    public static void bfs(int n) {
-        Queue <Integer> q = new LinkedList<>();
-        boolean visited[] = new boolean[N + 1];
-        visited[n] = true;
-        q.offer(n);
+
+    private static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[N + 1];
+
+        q.offer(start);
+        visited[start] = true;
 
         while (!q.isEmpty()) {
-            int parent = q.poll();
-            for (int i : list[parent]) {
-                if (visited[i]) {
-                    continue;
-                }
-                parentsArr[i] = parent;
-                visited[i] = true;
-                q.add(i);
+            int curNode = q.poll();
+
+            for (int nextNode : tree[curNode]) {
+                if (visited[nextNode]) continue;
+
+                parent[nextNode] = curNode;
+                visited[nextNode] = true;
+                q.offer(nextNode);
             }
+        }
+        for (int i = 2; i < N + 1; i++) {
+            System.out.println(parent[i]);
         }
     }
 }
