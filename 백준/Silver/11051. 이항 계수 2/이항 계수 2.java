aced02ca5ffde18472d5da8e998790e1d51a7144
soeun135 <84930396+soeun135@users.noreply.github.com>
2025-07-08 14:26:33 +0900

    [Silver II] Title: 이항 계수 2, Time: 192 ms, Memory: 21640 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -7,19 +7,17 @@ class Main {
         int N = scanner.nextInt();
         int K = scanner.nextInt();
 
-        long[][] D = new long[N + 1][N + 1];
+        int[][] D = new int[N + 1][N + 1];
 
         for (int i = 0; i <= N; i++) {
             D[i][0] = 1;
             D[i][i] = 1;
-            D[i][1] = i % 10007;
+            D[i][1] = i;
         }
 
         for (int i = 2; i <= N; i++) {
             for (int j = 2; j < i; j++) {
                 D[i][j] = (D[i - 1][j - 1] + D[i - 1][j]) % 10007;
-
-                if (i == N && j == K) break;
             }
         }
         System.out.println(D[N][K]);
