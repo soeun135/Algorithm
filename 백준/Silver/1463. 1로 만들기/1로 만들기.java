bde3fcccc93100be37a9ebd3c600e95ec40cb607
soeun135 <84930396+soeun135@users.noreply.github.com>
2025-07-09 14:34:25 +0900

    [Silver III] Title: 1로 만들기, Time: 200 ms, Memory: 21628 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,11 +2,13 @@ import java.util.*;
 import java.io.*;
 
 class Main {
+    static int[] DP;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        int[] DP = new int[1000001];
+        DP = new int[1000001];
 
         Arrays.fill(DP, Integer.MAX_VALUE);
         DP[1] = 0;
@@ -14,14 +16,26 @@ class Main {
 
         for (int i = 3; i <= N; i++) {
 
-            if (i % 3 == 0) DP[i] = Math.min(DP[i], DP[i / 3]);
-            if (i % 2 == 0) DP[i] = Math.min(DP[i], DP[i / 2]);
-            DP[i] = Math.min(DP[i], DP[i - 1]);
+            DP[i] = DP[i - 1] + 1;
 
-            DP[i] += 1;
+            if (i % 3 == 0) DP[i] = Math.min(DP[i], DP[i / 3] + 1);
+            if (i % 2 == 0) DP[i] = Math.min(DP[i], DP[i / 2] + 1);
         }
+        //dp(N);
 
         System.out.println(DP[N]);
         sc.close();
+    }
+
+    private static int dp (int n) {
+        if (DP[n] != Integer.MAX_VALUE) {
+            return DP[n];
+        }
+
+        if (n % 3 == 0)  DP[n] = Math.min(DP[n], dp(n / 3));
+        if (n % 2 == 0)  DP[n] = Math.min(DP[n], dp(n / 2));
+        DP[n] = Math.min(DP[n], dp(n - 1));
+
+         return DP[n] += 1;
     }
 }
