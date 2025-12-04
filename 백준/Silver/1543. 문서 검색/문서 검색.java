cc5d2e29a553ad4b55512e3a080add0e1e9aba63
soeun135 <84930396+soeun135@users.noreply.github.com>
2025-12-04 16:56:25 +0900

    [Silver V] Title: 문서 검색, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -4,14 +4,16 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         String doc = br.readLine();
         String word = br.readLine();
-        
+
         int docLen = doc.length();
-        
-         doc = doc.replace(word, ""); 
-        
+
+        if (doc.length() >= word.length()) {
+            doc = doc.replace(word, "");
+        }
+
         System.out.println((docLen - doc.length()) / word.length());
         br.close();
     }
