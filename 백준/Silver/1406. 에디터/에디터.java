f50b7bcf091988b8cedb3a46ae7b7af22f1964a6
soeun135 <84930396+soeun135@users.noreply.github.com>
2025-12-08 20:17:23 +0900

    [Silver II] Title: 에디터, Time: 616 ms, Memory: 144120 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,54 +1,52 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
+        StringBuilder sb = new StringBuilder();
+        
+        String N = br.readLine();
+        
         int M = Integer.parseInt(br.readLine());
-
-        Stack<Character> leftStack = new Stack<>();
-        Stack<Character> rightStack = new Stack<>();
-
-        for (int i = 0; i < str.length(); i++) {
-            leftStack.push(str.charAt(i));
+        
+        Stack<Character> lStack = new Stack<>();
+        Stack<Character> rStack = new Stack<>();
+        
+        for (int i = 0; i < N.length(); i++) {
+            lStack.push(N.charAt(i));
         }
-        StringTokenizer st;
         for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
             char cmd = st.nextToken().charAt(0);
-
-            switch (cmd) {
+            
+            switch(cmd) {
                 case 'L':
-                    if (!leftStack.isEmpty())
-                        rightStack.push(leftStack.pop());
+                    if (!lStack.isEmpty())
+                        rStack.push(lStack.pop());
                     break;
                 case 'D':
-                    if (!rightStack.isEmpty())
-                        leftStack.push(rightStack.pop());
+                    if (!rStack.isEmpty())
+                        lStack.push(rStack.pop());
                     break;
                 case 'B':
-                    if (!leftStack.isEmpty())
-                        leftStack.pop();
+                    if (!lStack.isEmpty())
+                        lStack.pop();
                     break;
                 case 'P':
-                    leftStack.push(st.nextToken().charAt(0));
+                    lStack.push(st.nextToken().charAt(0));
                     break;
             }
         }
-        StringBuilder sb = new StringBuilder();
-        
-        for (int i = leftStack.size(); i > 0; i--) {
-            sb.append(leftStack.pop());
+        while (!lStack.isEmpty()) {
+            rStack.push(lStack.pop());
         }
-        System.out.print(sb.reverse());
-        
-        sb = new StringBuilder();
-        for (int i = rightStack.size(); i > 0; i--) {
-            sb.append(rightStack.pop());
+        while (!rStack.isEmpty()) {
+            sb.append(rStack.pop());
         }
-
-        System.out.print(sb);
+        
+        System.out.println(sb.toString());
+        br.close();
     }
 }
