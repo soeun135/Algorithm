6b65a93aeeca0af17dfe9f7bdd8e3e6ac8cb55ef
soeun135 <84930396+soeun135@users.noreply.github.com>
2025-12-10 19:42:32 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 268 ms, Memory: 16796 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -11,31 +11,24 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[n];
         int outCnt = 1;
-        int idx = k - 1;
 
-        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new ArrayDeque<>();
 
         for (int i = 0; i < n; i++){
-            //arr[i] = i + 1;
             q.offer(i + 1);
         }
         sb.append("<");
-        arr[idx] = 0;
 
-
-        while (!q.isEmpty()){
+        while (q.size() > 1){
 
             for (int i = 1; i < k; i++) {
                 q.offer(q.poll());
             }
-            sb.append(q.poll());
-
-            if (!q.isEmpty()) sb.append(", ");
+            sb.append(q.poll()).append(", ");
         }
 
-        sb.append(">");
+        sb.append(q.poll()).append(">");
         System.out.println(sb);
         br.close();
     }
